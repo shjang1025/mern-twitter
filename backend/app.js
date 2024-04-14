674d24c08eb1af233cdde42856f218f1227be539
shjang1025 <shjang1025@gmail.com>
2024-04-14 00:34:17 -0700

    Express Error Handling

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const debug = require('debug')
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -35,8 +36,30 @@ app.use(
         }
     })
 )
+app.use((req, res, next) => {
+    const err = new Error('Not Found')
+    err.statusCode = 404;
+    next(err);
+})
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
 
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter)
 app.use('/api/csrf' , csrfRouter)
+
+
 module.exports = app;
