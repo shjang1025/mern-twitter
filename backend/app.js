8266b5f5acd8b8d5a95809ff6652f2e116357358
shjang1025 <shjang1025@gmail.com>
2024-04-14 00:31:53 -0700

    Express Server Security

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets')
+const csrfRouter = require('./routes/api/csrf')
+
 const app = express();
 
 app.use(logger('dev'));
@@ -11,7 +17,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:5173). (In production, React files
+    // will be served statically on the Express server.)
+    app.use(cors());
+  }
+
+
+
+app.use(
+    csurf({
+        cookie: {
+            secure : isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+)
+
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter)
-
+app.use('/api/csrf' , csrfRouter)
 module.exports = app;
